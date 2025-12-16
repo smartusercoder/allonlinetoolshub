e02feee35030e5f392d5be2ec4a8a09f3edd50de
smartusercoder <onlinebusinessemail123@gmail.com>
2025-12-16 13:43:07 -0500

    Online tools directory

--- wonder-werk-shop-final/src/hooks/useFavorites.ts
+++
@@ -1,3 +0,0 @@
-// This hook has been moved to contexts/FavoritesContext.tsx
-// Import from there instead: import { useFavorites } from "@/contexts/FavoritesContext"
-export { useFavorites } from "@/contexts/FavoritesContext";
